db17ced47d491846f8cdd3d1ef36f65e2682e302
SamaraDcruz <samaragracedcruz@gmail.com>
2025-09-13 05:07:10 -0400

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,15 @@
+# 1. Create an empty list
+my_list = []
+print("Empty list:", my_list)
+
+# 2. Create a list with elements
+my_list = [1, 2, 3, 4, 5]
+print("List with elements:", my_list)
+
+# 3. Use * operator (repeat the list)
+repeat_list = my_list * 2
+print("List after using * operator:", repeat_list)
+
+# 4. Reverse the list
+my_list.reverse()
+print("Reversed list:", my_list)
